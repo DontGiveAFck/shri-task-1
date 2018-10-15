5a22cef96da6f9ea02139889dd931e1abdb1fc0e
Arsen <simf.arsenio@gmail.com>
2018-10-16 02:31:32 +0300

    server.js refactored

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 const express = require('express');
+
 const app = express();
 const PORT = 8000;
 const cors = require('cors');
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // to prevent CORS error
 app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
 app.use(express.static('./public'));
 
-//routes require
+// routes require
 app.use('/', require('./server/routes/status'));
 app.use('/api', require('./server/routes/api'));
 
-app.use(function(req, res, ){
+app.use((req, res) => {
     res.status(404).send('<h1>Page not found</h1>');
 });
 
