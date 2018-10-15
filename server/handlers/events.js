ddec2010b519328e8e699cc878f13164a4e30e05
Arsen <simf.arsenio@gmail.com>
2018-10-16 01:50:45 +0300

    refactored server code, fixed sound diagram

--- server/handlers/events.js
+++ server/handlers/events.js
@@ -1,26 +1,27 @@
 const fs = require('fs');
+
 const error = {
-    incorrectType: 'incorrect type'
-}
+    incorrectType: 'incorrect type',
+};
 const eventTypes = ['info', 'critical'];
 
 module.exports = {
-    getEvents: function (req, res) {
+    getEvents(req, res) {
         // for pagination
-        let limit = req.query.limit || req.body.limit || Number.MAX_SAFE_INTEGER;
-        let offset = req.query.offset || req.body.offset || 0;
+        const limit = req.query.limit || req.body.limit || Number.MAX_SAFE_INTEGER;
+        const offset = req.query.offset || req.body.offset || 0;
 
-        //if type specified
-        let type = req.query.type || req.body.type;
-        if (req.query.type) {
-            let types = req.query.type.split(':');
+        // if type specified
+        const type = req.query.type || req.body.type;
+        if (type) {
+            const types = req.query.type.split(':');
             if (this._isTypesValid(types)) {
-                let result = [];
+                const result = [];
                 fs.readFile(__dirname.concat('/../files/events.json'), 'utf8', (err, data) => {
                     if (err) console.log('Smth wrong with file');
-                    let dataObj = JSON.parse(data);
+                    const dataObj = JSON.parse(data);
                     dataObj.events.forEach((event) => {
-                        if(types.includes(event.type)) {
+                        if (types.includes(event.type)) {
                             result.push(event);
                         }
                     });
@@ -34,34 +35,33 @@ module.exports = {
         } else {
             fs.readFile(__dirname.concat('/../files/events.json'), 'utf8', (err, data) => {
                 if (err) throw err;
-                let dataObj = JSON.parse(data);
+                const dataObj = JSON.parse(data);
                 res.status(200).json(this._pagination(dataObj.events, limit, offset));
             });
         }
     },
 
-    _isTypesValid: function (types) {
+    _isTypesValid(types) {
         let result = true;
         types.forEach((type) => {
-            if(!(eventTypes.includes(type))) {
+            if (!(eventTypes.includes(type))) {
                 result = false;
-                return;
             }
-        })
+        });
         return result;
     },
 
-    _pagination: function (allEventsArray, limit, offset) {
-        let res = [];
+    _pagination(allEventsArray, limit, offset) {
+        const res = [];
         let count = 0;
         allEventsArray.forEach((event, i) => {
             if (i + 1 > offset && count < limit) {
-                res.push(event)
-                count++;
+                res.push(event);
+                count += count;
             }
         });
 
         return res;
-    }
+    },
 
-}
+};
--- server/handlers/status.js
+++ server/handlers/status.js
@@ -1,13 +1,14 @@
 module.exports = {
-    getUptime: function (req, res) {
+    getUptime(req, res) {
         res.send(`<h1>Uptime: ${this._getValidTime(Math.floor(process.uptime()))}</h1>`);
     },
 
-    _getValidTime: function (sec) {
-        let hours = Math.floor(sec / 3600);
+    _getValidTime(input) {
+        let sec = input;
+        const hours = Math.floor(sec / 3600);
         sec %= 3600;
-        let minutes = Math.floor(sec / 60);
-        let seconds = sec % 60;
+        const minutes = Math.floor(sec / 60);
+        const seconds = sec % 60;
         return `${hours}:${minutes}:${seconds}`;
-    }
-}
+    },
+};
