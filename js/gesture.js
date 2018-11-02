dfcf4abdfc5bf179bebe7d3eae3f513af4bf3742
Arsen <simf.arsenio@gmail.com>
2018-11-02 23:51:54 +0300

    changed gesture

--- js/gesture.js
+++ js/gesture.js
@@ -1,69 +1,160 @@
 (function() {
-    var wrapper = document.body.querySelector('.image-wrapper'),
-        transVal = document.body.querySelector('.trans span'),
-        zoomVal = document.body.querySelector('.zoom span'),
-        brightVal = document.body.querySelector('.brightness span');
-    if (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0 || navigator.maxTouchPoints > 0)) {
-            var touch = null,
-            pointers = [];
+    const image = document.querySelector('.image-wrapper');
+    const currentPointerEvents = {};
+    const imageState = {
+        leftMin: -1000,
+        left: 0,
+        leftMax: 1000,
+        zoomMin: 100,
+        zoom: 100,
+        zoomMax: 300,
+        brightnessMin: .2,
+        brightness: 1,
+        brightnessMax: 4,
+    };
 
-        wrapper.addEventListener('pointerdown', (event) => {
-            wrapper.setPointerCapture(event.pointerId);
-            pointers.push(event);
-            touch = {
-                startX: event.x,
-                prevX: event.x
+    // Описание текущего жеста
+    let gesture = null;
+
+    // Запрещает таскать картинку мышкой
+    image.addEventListener('dragstart', (event) => { event.preventDefault() });
+
+    image.addEventListener('pointerdown', (event) => {
+        console.log('down')
+        currentPointerEvents[event.pointerId] = event;
+        if (!gesture) {
+            gesture = {
+                type: 'move',
             }
-        });
-
-        wrapper.addEventListener('pointerup', () => {
-            pointers = [];
-            touch = null;
-        });
-
-        wrapper.addEventListener('pointermove', (event) => {
-            if (pointers.length == 2) {
-                brightness(event);
-            } else if (pointers.length == 1) {
-                rotation(event);
-            } else if (pointers.length > 2) {
-                zoom(event);
-            }
-        });
-    }
-
-    function rotation(event) {
-        var posX = parseInt(wrapper.style.backgroundPositionX) || 0,
-            slideLength = 0;
-
-        wrapper.style.backgroundPositionX = posX ? posX : '1px';
-        slideLength = touch.startX < event.clientX ?
-            (posX + event.clientX / 20):
-            (posX - event.clientX / 20);
-        wrapper.style.backgroundPositionX =
-            Math.abs(parseInt(slideLength)) > wrapper.width ?
-            `${wrapper.width}px` :
-            `${slideLength}px`;
-
-        transVal.innerText = `${ Math.abs(posX) > 360 ? 360 : posX }deg`;
-    }
-
-    function zoom(event) {
-        var posX = parseInt(wrapper.style.backgroundSize) || 0;
-        if (posX) {
-            wrapper.style.backgroundSize = `${posX + (event.clientX / 10)}px ${posX + (event.clientX / 10)}px`;
-        } else {
-            wrapper.style.backgroundSize = '1px'
         }
-        zoomVal.innerText = `${ posX / 100 }%`;
+    });
+
+    const getDistance = (e1, e2) => {
+        const {clientX: x1, clientY: y1} = e1;
+        const {clientX: x2, clientY: y2} = e2;
+        return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
     }
 
-    function brightness(event) {
-        var bright = (Math.atan(event.clientY / event.clientX) * 180 / Math.PI) / 100;
-        var minBright = 0.2;
-        var maxBright = 2;
-        bright = bright < minBright ? minBright : (bright > maxBright ? maxBright : bright);
-        wrapper.style.filter = `brightness(${ bright })`;
-        brightVal.innerText = `${ parseInt(bright * 100) }%`;
+    const getAngle = (e1, e2) => {
+        const {clientX: x1, clientY: y1} = e1;
+        const {clientX: x2, clientY: y2} = e2;
+        const r = Math.atan2(x2 - x1, y2 - y1);
+        return 360 - (180 + Math.round(r * 180 / Math.PI));
     }
+
+    const feedbackNodes = {
+        left: document.body.querySelector('.trans span'),
+        zoom: document.body.querySelector('.zoom span'),
+        brightness: document.body.querySelector('.brightness span'),
+    }
+
+    const setFeedback = (name, value) => {
+        feedbackNodes[name].innerText = Math.round(value * 100) / 100;
+    }
+
+    const setLeft = (dx) => {
+        const {leftMin, leftMax} = imageState;
+
+        imageState.left += dx;
+        if (imageState.left < leftMin) {
+            imageState.left = leftMin;
+        } else if (imageState.left > leftMax) {
+            imageState.left = leftMax;
+        }
+        image.style.backgroundPositionX = `${imageState.left}px`;
+
+        setFeedback('left', -imageState.left);
+    }
+
+    image.addEventListener('pointermove', (event) => {
+        const pointersCount = Object.keys(currentPointerEvents).length;
+
+        if (pointersCount === 0 || !gesture) {
+            return;
+        }
+
+        if (pointersCount === 1 && gesture.type === 'move' && !currentPointerEvents.fake) {
+            const previousEvent = currentPointerEvents[event.pointerId];
+            const dx = event.clientX - previousEvent.clientX;
+            setLeft(dx);
+            currentPointerEvents[event.pointerId] = event;
+        } else if (pointersCount === 2) {
+
+            currentPointerEvents[event.pointerId] = event;
+            const events = Object.values(currentPointerEvents);
+            const distance = getDistance(events[0], events[1]);
+            const angle = getAngle(events[0], events[1]);
+
+            if (!gesture.startDistance) {
+                gesture.startZoom = imageState.zoom;
+                gesture.startDistance = distance;
+
+                gesture.startBrightness = imageState.brightness;
+                gesture.startAngle = angle;
+                gesture.angleDiff = 0;
+                gesture.type = null;
+            }
+
+            const diff =  distance - gesture.startDistance;
+            const angleDiff = angle - gesture.startAngle;
+
+            if(!gesture.type) {
+                if (Math.abs(diff) < 32 && Math.abs(angleDiff) < 8) {
+                    return;
+                } else if (Math.abs(diff) > 32) {
+                    gesture.type = 'zoom';
+                } else {
+                    gesture.type = 'rotate';
+                }
+            }
+
+            if (gesture.type === 'zoom') {
+                const {zoomMin, zoomMax} = imageState;
+                let zoom =  gesture.startZoom + diff;
+                if (diff < 0) {
+                    zoom = Math.max(zoom, zoomMin);
+                } else {
+                    zoom = Math.min(zoom, zoomMax);
+                }
+
+                imageState.zoom = zoom;
+                image.style.backgroundSize = `${zoom}%`;
+                setFeedback('zoom', zoom);
+            }
+
+            if (gesture.type === 'rotate') {
+                const {brightnessMin, brightnessMax} = imageState;
+
+                if (Math.abs(angleDiff - gesture.angleDiff) > 300) {
+                    gesture.startBrightness = imageState.brightness;
+                    gesture.startAngle = angle;
+                    gesture.angleDiff = 0;
+                    return;
+                }
+
+                gesture.angleDiff = angleDiff;
+
+                let brightness = gesture.startBrightness + angleDiff / 50;
+                if (angleDiff < 0) {
+                    brightness = Math.max(brightness, brightnessMin);
+                } else {
+                    brightness = Math.min(brightness, brightnessMax);
+                }
+
+                imageState.brightness = brightness;
+                image.style.filter = `brightness(${brightness})`;
+                setFeedback('brightness', brightness);
+            }
+        }
+
+    });
+
+    const onPointerUp = (event) => {
+        gesture = null;
+        delete currentPointerEvents[event.pointerId];
+    }
+
+    image.addEventListener('pointerup', onPointerUp);
+    image.addEventListener('pointercancel', onPointerUp);
+    image.addEventListener('pointerleave', onPointerUp);
 })();
--- js/template.js
+++ js/template.js
@@ -210,16 +210,12 @@
             if (event.data.image) {
                     var img = eventContainer.querySelector('.image');
                     img.setAttribute('src', 'images/md.png');
-                    img.setAttribute('srcset', 'images/sm.png 832w, images/lg.png 2496w');
-                    img.setAttribute('sizes', '(max-width: 648px) 832px, (min-width: 1600) 2496px');
-
-                    if(isTouchDevice() && !isImageAdded) {
-                        isImageAdded = true;
-                        var wrapper = eventContainer.querySelector('.image-wrapper');
-                        wrapper.style.backgroundImage = 'url("images/sm.png")';
-                        wrapper.style.width = '100%';
-                        img.style.visibility = 'hidden';
-                        img.style.pointerEvents = 'none';
+                    var wrapper = eventContainer.querySelector('.image-wrapper');
+                    wrapper.style.backgroundImage = 'url("images/sm.png")';
+                    wrapper.style.width = '100%';
+                    img.style.visibility = 'hidden';
+                    img.style.pointerEvents = 'none';
+                    if(isTouchDevice()) {
                         eventContainer.querySelector('.image-info').style.display = 'flex';
                     }
             } else {
