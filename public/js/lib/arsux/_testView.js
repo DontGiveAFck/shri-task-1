d0b5105b54b70dc9e6e9efd4fb625ea76a1788e0
Arsen <simf.arsenio@gmail.com>
2018-11-05 20:13:35 +0300

    store added with usage example

--- public/js/lib/arsux/_testView.js
+++ public/js/lib/arsux/_testView.js
@@ -1,46 +1,59 @@
 import Store from './store.js';
-import Dispatcher from './dispatcher.js';
 
-const store = new Store();
-
-const dispatcher = new Dispatcher(store);
-
-// create an action
-const events = [
-    {
-        id: 1,
-        device: 'TV',
-    },
-];
-
-store.subscribe((state, action) => {
+// create a reducer
+const reducer = (state, action) => {
     switch (action.type) {
-    case 'ADD_EVENTS': {
-        state.events = events;
+    case 'INIT_EVENTS': {
+        state.events = action.payload;
         break;
     }
     case 'REMOVE_EVENT': {
-        state.events = state.events.splice(action.payload.eventId, 1);
+        state.events.splice(action.payload, 1);
         break;
     }
     default: return state;
     }
-});
-console.log('state', store.getState());
-dispatcher.dispatch({
-    type: 'ADD_EVENTS',
-    data: {
-        eventId: 1,
-    },
+};
+
+// create store, add reducers to the store
+const store = new Store(reducer);
+
+// create a view controller
+const viewController = (state) => {
+    // events from the state
+    const currentEvents = state.events;
+
+    // do smth with events
+    console.log('events', currentEvents);
+};
+
+// subscribe to state update
+store.subscribe('INIT_EVENTS', viewController);
+store.subscribe('REMOVE_EVENT', viewController);
+
+// dispatch an action
+store.dispatch({
+    type: 'INIT_EVENTS',
+    payload: [
+        {
+            id: '1',
+            resource: 'Пылесос',
+            time: '12.00',
+        },
+        {
+            id: '2',
+            resource: 'Холодильник',
+            time: '12.00',
+        },
+        {
+            id: '3',
+            resource: 'Зубочистка',
+            time: '12.00',
+        },
+    ],
 });
 
-console.log(store.getState());
-
-dispatcher.dispatch({
+store.dispatch({
     type: 'REMOVE_EVENT',
-    data: {
-        eventId : 1,
-    },
+    payload: 2,
 });
-
-console.log(store.getState());
--- public/js/lib/arsux/store.js
+++ public/js/lib/arsux/store.js
@@ -1,20 +1,39 @@
 export default class Store {
-    constructor() {
-        this.subscribers = [];
+    constructor(...reducers) {
+        // view controllers, that listen for state change
+        this.subscribers = []; // example: [{actionType: 'action1', cb: doSomething()}]
+
+        // functions, that describe how state will change on specified action
+        this.reducers = [...reducers];
+
+        // current state
         this.state = {};
     }
 
-    subscribe(callback) {
-        this.subscribers.push(callback);
+    dispatch(action) {
+        // 1. change state
+        this.reducers.forEach((reducer) => {
+            reducer(this.state,  action);
+        });
+
+        // 2. tell to all subscribers, that state has changed
+        this.subscribers.forEach((subscriber) => {
+            if (subscriber.actionType === action.type) {
+                subscriber.cb(this.state);
+            }
+        });
     }
 
-    emitStateChange(action) {
-        this.subscribers.forEach(cb => cb(this.state, action));
+    // subscribe ViewController to state changing with specified action
+    subscribe(type, viewController) {
+        this.subscribers.push({
+            actionType: type,
+            cb: viewController,
+        });
     }
 
+    // get current state
     getState() {
         return this.state;
     }
-
-
 }
